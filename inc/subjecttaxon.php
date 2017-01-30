e6ddafc8fb4bb4d11008ce07336478d1afba9b7a
Phil Barker <phil.barker@hw.ac.uk>
2017-01-30 16:03:57 +0000

    adding subject taxonomy

---
+++ inc/subjecttaxon.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * create a custom taxonomy for subjects
+ **
+ * see https://codex.wordpress.org/Taxonomies for example on which this is based
+ **/
+
+add_action( 'init', '' );
+function pubwp_subject_init() {
+	// create taxonomy called pubwp_subject
+	register_taxonomy(
+		'subjects',
+		'post',
+		array(
+			'label'   => __( 'Subjects', 'pubwp' ),
+			'rewrite' => array( 'slug' => 'subject' ),
+		)
+	);
+}
+ 
