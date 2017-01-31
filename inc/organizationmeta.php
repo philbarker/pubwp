54facbafed33ba2e52879686c0e5d1137a393073
Phil Barker <phil.barker@hw.ac.uk>
2017-01-31 15:29:28 +0000

    better name for function, ref #9

--- inc/organizationmeta.php
+++ inc/organizationmeta.php
@@ -76,8 +76,8 @@ function pubwp_register_organization_meta_boxes( $meta_boxes ) {
 
 // Organization custom post type does not support title, but do need a title to display
 // in selection boxes when linking to an organization from another post.
-add_filter( 'wp_insert_post_data', 'pubwp_modify_post_title', 10, 1 );
-function pubwp_modify_post_title( $data ) {
+add_filter( 'wp_insert_post_data', 'pubwp_modify_organization_title', 10, 1 );
+function pubwp_modify_organization_title( $data ) {
 	$prefix = '_pubwp_organization_';
 	if ('pubwp_organization' == $data['post_type'] && rwmb_meta( "{$prefix}name" ) ) 
 		$data['post_title'] = rwmb_meta( "{$prefix}name" );
