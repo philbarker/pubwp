0510cb6e24b9a047d36c4b1eef6cb1ffdf1d113b
Phil Barker <phil.barker@hw.ac.uk>
2017-03-07 17:16:31 +0000

    code review presentation, for #35

--- inc/presentationmeta.php
+++ inc/presentationmeta.php
@@ -17,18 +17,18 @@ defined( 'ABSPATH' ) or die( 'Be good. If you can\'t be good be careful' );
 
 add_action( 'init', 'pubwp_create_presentation_type' );
 function pubwp_create_presentation_type() {
-  register_post_type( 'pubwp_presentation',
-    array(
-      'labels' => array(
-        'name' => __( 'Presentations', 'pubwp' ),
-        'singular_name' => __( 'Presentation', 'pubwp' )
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'rewrite' => array('slug' => 'presentation'),
-      'supports' => array('title' ,'revisions' )
-    )
-  );
+ 	register_post_type( 'pubwp_presentation',
+		array(
+			'labels' => array(
+				'name' => __( 'Presentations', 'pubwp' ),
+				'singular_name' => __( 'Presentation', 'pubwp' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		'rewrite' => array('slug' => 'presentation'),
+		'supports' => array('title' ,'revisions' )
+		)
+	);
 }
 
 // commonmeta.php provides a metabox for pubwp_presentation which includes
@@ -38,15 +38,15 @@ function pubwp_create_presentation_type() {
 // More info @ http://metabox.io/docs/registering-meta-boxes/
 add_filter( 'rwmb_meta_boxes', 'pubwp_register_presentation_meta_boxes' );
 function pubwp_register_presentation_meta_boxes( $meta_boxes ) {
-    // @param array $meta_boxes List of meta boxes
-    // @return array
+	// @param array $meta_boxes List of meta boxes
+	// @return array
 	$prefix = '_pubwp_presentation_';  // prefix of meta keys keys hidden
 
 
 	$meta_boxes[] = array(
 		'id'         => 'pubwp_presentation_info',  // Meta box id
 		// Meta box title - Will appear at the drag and drop handle bar. Required.
-        'title'      => __( 'Presentation information', 'pubwp' ),
+		'title'      => __( 'Presentation information', 'pubwp' ),
 		'post_types' => array( 'pubwp_presentation' ),// Post types that have this metabox
 		'context'    => 'normal',             // Where the meta box appear
 		'priority'   => 'low',               // Order of meta box
@@ -80,51 +80,61 @@ function pubwp_register_presentation_meta_boxes( $meta_boxes ) {
 
 function pubwp_print_meeting_info( ) {
 	$prefix = '_pubwp_presentation_';
-	if ( empty( rwmb_meta( "{$prefix}conference_name", 'text' ) ) ) {
+	$args = array( 'type' => 'text' );
+	$conference_name = rwmb_meta( "{$prefix}conference_name", $args );
+	$conference_location = rwmb_meta( "{$prefix}conference_location", $args );
+	$conference_dates = rwmb_meta( "{$prefix}dates", $args );
+
+	if ( empty( $conference_name ) ) {
 			$conference_name = False; 
 		} else {
-			$conference_name = rwmb_meta( "{$prefix}conference_name", 'text' );
+			$conference_name = esc_html( $conference_name );
 		}
-	if ( empty( rwmb_meta( "{$prefix}conference_location", 'text' ) ) ) {
+	if ( empty( $conference_location ) ) {
 			$conference_location = False; 
 		} else {
-			$conference_location = rwmb_meta( "{$prefix}conference_location", 'text' );
+			$conference_location = esc_html( $conference_location );
 		}
-	if ( empty( rwmb_meta( "{$prefix}dates", 'text' ) ) ) {
+	if ( empty( $conference_dates ) ) {
 			$conference_dates = False; 
 		} else {
-			$conference_dates = rwmb_meta( "{$prefix}dates", 'text' );
+			$conference_dates = esc_html( $conference_dates );
 		}
 
 	if ($conference_name) {
-	echo "<span property='recordedAt' typeof='Event'>
-	      <span property='name'>{$conference_name}</span>";
+		echo "<span property='recordedAt' typeof='Event'>
+		<span property='name'>{$conference_name}</span>";
 		if ($conference_location) { 
-	    	echo ", <span property='place'>{$conference_location}</span> ";
-	    }
-	    if ($conference_dates) {
-	      echo " ({$conference_dates}).";
-	    }
+			echo ", <span property='place'>{$conference_location}</span> ";
+		}
+		if ($conference_dates) {
+			echo " ({$conference_dates}).";
+		}
 	echo "</span>";
 	}	
 }
 
 function pubwp_presentation_info( $post ) {
 	$prefix = '_pubwp_presentation_';
-	$post_id = $post->ID;
+	$args = array( 'type' => 'text' );
+	$conference_name = rwmb_meta( "{$prefix}conference_name", $args, $post->ID );
+	$conference_location = rwmb_meta( "{$prefix}conference_location", $args, $post->ID );
+	$conference_dates = rwmb_meta( "{$prefix}dates", $args, $post->ID );
 	$presentation_info = '';
-	if ( ! empty( rwmb_meta( "{$prefix}conference_name", 'text', $post_id ) ) ) {
-			$conference_name = rwmb_meta( "{$prefix}conference_name", 'text',  $post_id);
+	
+	if ( ! empty( $conference_name ) ) {
 			$presentation_info = $presentation_info.'Presented at '.$conference_name;
 	}
-	if ( ! empty( rwmb_meta( "{$prefix}conference_location", 'text',  $post_id ) ) ) {
-			$conference_location = rwmb_meta( "{$prefix}conference_location", 'text',  $post_id );
+	if ( ! empty( $conference_location ) ) {
 			$presentation_info = $presentation_info.', '.$conference_location;
 	}
-	if ( ! empty( rwmb_meta( "{$prefix}dates", 'text' ) ) ) {
-			$conference_dates = rwmb_meta( "{$prefix}dates", 'text', $post_id );
-			$presentation_info = $presentation_info.' '.$conference_dates;
+	if ( ! empty( $conference_dates ) ) {
+		$presentation_info = $presentation_info.' '.$conference_dates;
 	}
-	return esc_html($presentation_info);
+	if (! empty( $presentation_info ) ) {
+		return esc_html( $presentation_info );
+	} else {
+		return false;
+	}	
 }
 
