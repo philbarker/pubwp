5a5eadd096ca74646d466e4f159e189a824bead6
Phil Barker <phil@pjjk.net>
2017-03-12 14:21:59 +0000

    tweaks from code review of licence taxonomy. #35

--- inc/licencetaxon.php
+++ inc/licencetaxon.php
@@ -9,17 +9,19 @@ defined( 'ABSPATH' ) or die( 'Be good. If you can\'t be good be careful' );
 
 add_action( 'init', 'pubwp_licence_init' );
 function pubwp_licence_init() {
-	// create taxonomy called pubwp_subject
+	// create taxonomy called pubwp_licence
 	register_taxonomy(
 		'licences',
-		array(  'pubwp_book',  // Post types that have subject taxonomy
-		        'pubwp_report',
-		        'pubwp_presentation',
-		        'pubwp_chapter',
-			'pubwp_paper' ),
+		array(
+			'pubwp_book', // Post types that have licence taxonomy
+			'pubwp_report',
+			'pubwp_presentation',
+			'pubwp_chapter',
+			'pubwp_paper'
+		),
 		array(
 			'label'   => __( 'Licences', 'pubwp' ),
-			'rewrite' => array( 'slug' => 'licence' ),
+			'rewrite' => array( 'slug' => 'licence' )
 		)
 	);
 }
