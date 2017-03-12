8da2053bc6e0f4ee8ab8d3de9280a91dc4a4b771
Phil Barker <phil@pjjk.net>
2017-03-12 14:24:27 +0000

    tweaks from code review of subject taxonomy. Completes this code review, closes #35

--- inc/subjecttaxon.php
+++ inc/subjecttaxon.php
@@ -12,11 +12,13 @@ function pubwp_subject_init() {
 	// create taxonomy called pubwp_subject
 	register_taxonomy(
 		'subjects',
-		array(  'pubwp_book',  // Post types that have subject taxonomy
-		        'pubwp_report',
-		        'pubwp_presentation',
-		        'pubwp_chapter',
-			'pubwp_paper' ),
+		array(
+			'pubwp_book',  // Post types that have subject taxonomy
+			'pubwp_report',
+			'pubwp_presentation',
+			'pubwp_chapter',
+			'pubwp_paper' 
+		),
 		array(
 			'label'   => __( 'Subjects', 'pubwp' ),
 			'rewrite' => array( 'slug' => 'subject' ),
